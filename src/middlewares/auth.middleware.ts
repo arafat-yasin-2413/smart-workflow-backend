3ed5a1444472dabd42c6121e4d6e8af573241a66
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-21 00:47:02 +0600

    adds: middleware and defines express.d.ts files

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "../config/env";
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "You are Unauthorized" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, env.jwtAccessSecret);
+        req.user = decoded;
+        next();
+    } catch {
+        res.status(401).json({ message: "Invalid token" });
+    }
+};
