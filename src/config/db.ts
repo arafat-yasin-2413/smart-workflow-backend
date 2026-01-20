83886aa42e4996387f3e6911637635f2e08d0ba4
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-20 21:28:24 +0600

    creates db.ts file

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import {env} from "./env";
+
+export const connectDB =async() =>{
+    try{
+        await mongoose.connect(env.mongodbUri);
+        console.log('✅ MongoDB connected');
+    }
+    catch(error:any){
+        console.log("❌ DB connection failed");
+        process.exit(1);
+    }
+};
--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,9 @@ dotenv.config();
 
 export const env = {
     port: process.env.PORT!,
-}
+    mongodbUri: process.env.MONGODB_URI!,
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET!,
+    jwtRefreshSecret: process.env.JWT_REFRESH_SECRET!,
+    accessExpires: process.env.ACCESS_EXPIRES!,
+    refreshExpires: process.env.REFRESH_EXPIRES!
+};
