0cf5fda1bc126ac1311a4e23c10a24a120e669cd
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-21 11:45:39 +0600

    add: defines loginUser auth.service function

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
-import { RegisterDTO } from "./auth.types";
+import { LoginDTO, RegisterDTO } from "./auth.types";
 import { User } from "../user/user.model";
+import { signAccessToken, signRefreshToken } from "../../utils/jwt";
 
 export const registerUser = async (payload: RegisterDTO) => {
     const isExist = await User.findOne({ email: payload.email });
@@ -16,4 +17,33 @@ export const registerUser = async (payload: RegisterDTO) => {
     });
 
     return user;
+};
+
+export const loginUser = async (payload: LoginDTO) => {
+    const user = await User.findOne({ email: payload.email });
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    if (!user.password) {
+        throw new Error("User password not set");
+    }
+
+    const isMatch = await bcrypt.compare(payload.password, user.password);
+    if (!isMatch) {
+        throw new Error("Invalid Password");
+    }
+
+    const accessToken = signAccessToken({
+        userId: user._id.toString(),
+        role: user.role,
+    });
+
+    const refreshToken = signRefreshToken({
+        userId: user._id.toString(),
+        role: user.role,
+    });
+
+    return { accessToken, refreshToken };
 };
