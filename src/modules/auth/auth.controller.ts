855c78afbac6f978a433febb5f3fe2a5e46f997a
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-01-21 11:53:27 +0600

    fix: fixes with return keyword in register and login function in auth.controller file

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from "express";
-import { registerUser } from "./auth.service";
+import { loginUser, registerUser } from "./auth.service";
 
 export const regiser = async (req: Request, res: Response) => {
     try {
         const user = await registerUser(req.body);
-        res.status(201).json({ message: "User registered", user });
+        return res.status(201).json({ message: "User registered", user });
     } catch (error: any) {
-        res.status(400).json({ message: error.message });
+        return res.status(400).json({ message: error.message });
+    }
+};
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const tokens = await loginUser(req.body);
+        return res.json(tokens);
+    } catch (error: any) {
+        return res.status(401).json({ message: error.message });
     }
 };
